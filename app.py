4cd7e598341a29416a0440ea616d9c1c47576e55
Adam Hlavacek <git@adamhlavacek.com>
2024-12-02 08:42:23 +0100

    feat: search

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask, render_template
+import os
+from datetime import datetime
+from typing import List
+
+import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from sqlitedb import IndexedDBManager
+
+load_dotenv()
+NEWS_API_KEY = os.environ['NEWS_API_KEY']
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+db = IndexedDBManager('db.sqlite3')
+
+
+def perform_search(search_query: str):
+    articles = requests.get('https://newsapi.org/v2/everything',
+                            params={'q': search_query, 'apiKey': NEWS_API_KEY}).json()
+    db['searches'][search_query] = {
+        'search_query': search_query,
+        'searched_at': datetime.now().isoformat(),
+        'articles': articles
+    }
+
+
+def get_saved_search_queries() -> List[str]:
+    return list(map(
+        lambda x: x['search_query'],
+        sorted(db['searches'].values(), key=lambda x: datetime.fromisoformat(x['searched_at']), reverse=True)
+    ))
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():  # put application's code here
-    return render_template('base.html')
+def page_home():
+    search_query = request.args.get('search')
+    search = None
+    if search_query:
+        if search_query not in db['searches']:
+            perform_search(search_query)
+        search = db['searches'][search_query]
+
+    return render_template(
+        'base.html',
+        search_query=search_query,
+        search=search,
+        saved=get_saved_search_queries()
+    )
 
 
 if __name__ == '__main__':
